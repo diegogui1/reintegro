daaeb3ec7dc1b76266eeb9945bf0b6d8220ea6ea
Diego Guizzo <dguizzo@tgv.com.ar>
2024-12-03 17:26:50 -0300

    test

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,30 +1,30 @@
 const cds = require('@sap/cds');
- 
 
-module.exports = cds.service.impl(async function() {
-	const { factura } = this.entities;
- 
 
-	this.on("READ", factura, async (req, next) => {
-		 
+module.exports = cds.service.impl(async function () {
+    const { factura } = this.entities;
+
+
+    this.on("READ", factura, async (req, next) => {
+
         const tx = cds.transaction(req);
         try {
-      
+
 
             // Handover to the SF OData Service to fecth the requested data
-            
-    
-            const data =  await tx.run(SELECT.from("mi_factura_facturas"));
+
+
+            const data = await tx.run(SELECT.from("mi_factura_facturas"));
             return data;
         } catch (err) {
             req.error(err.code, err.message);
         }
- 
-	});
+
+    });
 
     this.on("CREATE", factura, async (req, next) => {
-		 
-	 
+
+
         const tx = cds.transaction(req);
 
         try {
@@ -36,25 +36,25 @@ module.exports = cds.service.impl(async function() {
             if (!idmsg || !idfile || !idthread) {
                 req.error(400, 'Missing required fields: idmsg, idfile, or idthread.');
             }
-        
+
             // Si todos los campos están presentes, realiza la inserción en la base de datos
-            const result = await tx.run( await INSERT.into("mi_factura_facturas").entries( req.data));
+            const result = await tx.run(await INSERT.into("mi_factura_facturas").entries(req.data));
+            console.info("insertar datos en sqlite");
             return result;
 
-           // return data.results;
-          } catch (err) {
-    
-            console.info (err);
-              req.res.json(err);
-    
-          }
- 
-	});
-	 
- 
-	 
+            // return data.results;
+        } catch (err) {
+
+            console.info(err);
+            req.res.json(err);
+
+        }
+
+    });
+
+
+
 
 });
 
- 
- 
+
